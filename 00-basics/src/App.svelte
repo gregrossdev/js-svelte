af65f1191d7ef1d063438376f22ee11ad9d36fca
Gregory Ross <gregross.dev@gmail.com>
2022-11-01 13:20:22 -0400

    nested components

--- 00-basics/src/App.svelte
+++ 00-basics/src/App.svelte
@@ -1,9 +1,11 @@
 <script>
     let src = 'https://svelte.dev/tutorial/image.gif';
     let name = 'Rick Astley';
+    import Nested from "./components/Nested.svelte";
 </script>
 
 <img {src} alt="{name} dances.">
+<Nested />
 
 <style>
     img {
