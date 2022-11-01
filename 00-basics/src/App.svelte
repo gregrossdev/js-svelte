ccfb4c9bdf294f4c2cc8f729c5c028ee1dd932bc
Gregory Ross <gregross.dev@gmail.com>
2022-11-01 13:27:21 -0400

    wiring up an event handler

--- 00-basics/src/App.svelte
+++ 00-basics/src/App.svelte
@@ -4,11 +4,20 @@
     let name = 'Rick Astley';
 
     let string = `this string contains some <strong>HTML!!!</strong>`;
+
+    let count = 0;
+
+    function incrementCount() {
+        count += 1;
+    }
 </script>
 
 <img {src} alt="{name} dances.">
 <Nested />
 <p>{@html string}</p>
+<button on:click={incrementCount}>
+    Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
 
 <style>
     img {
