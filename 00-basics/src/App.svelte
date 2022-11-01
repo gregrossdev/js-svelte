c4aca1270cbd02667ef204561a6bd44aafc78997
Gregory Ross <gregross.dev@gmail.com>
2022-11-01 13:22:34 -0400

    html tags

--- 00-basics/src/App.svelte
+++ 00-basics/src/App.svelte
@@ -1,11 +1,14 @@
 <script>
+    import Nested from "./components/Nested.svelte";
     let src = 'https://svelte.dev/tutorial/image.gif';
     let name = 'Rick Astley';
-    import Nested from "./components/Nested.svelte";
+
+    let string = `this string contains some <strong>HTML!!!</strong>`;
 </script>
 
 <img {src} alt="{name} dances.">
 <Nested />
+<p>{@html string}</p>
 
 <style>
     img {
