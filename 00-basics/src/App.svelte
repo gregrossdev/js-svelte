83a353deccd5d6f6cc4444e49f67ec00e8af516f
Gregory Ross <gregross.dev@gmail.com>
2022-11-01 13:29:17 -0400

    reactive declarations

--- 00-basics/src/App.svelte
+++ 00-basics/src/App.svelte
@@ -6,18 +6,20 @@
     let string = `this string contains some <strong>HTML!!!</strong>`;
 
     let count = 0;
+    $: doubled = count * 2;
 
     function incrementCount() {
         count += 1;
     }
 </script>
 
-<img {src} alt="{name} dances.">
+<!--<img {src} alt="{name} dances.">-->
 <Nested />
 <p>{@html string}</p>
 <button on:click={incrementCount}>
     Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
+<p>{count} doubled is {doubled}</p>
 
 <style>
     img {
