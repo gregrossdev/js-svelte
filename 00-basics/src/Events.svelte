3b4ff1553d09cc4b2477c693889bda578016eeb1
Gregory Ross <gregross.dev@gmail.com>
2022-11-02 09:32:21 -0400

    inline mouse event listener

--- 00-basics/src/Events.svelte
+++ 00-basics/src/Events.svelte
@@ -11,6 +11,10 @@
     The mouse position is {m.x} x {m.y}
 </div>
 
+<div on:mousemove="{e => m = { x: e.clientX, y: e.clientY }}">
+    The mouse position is {m.x} x {m.y}
+</div>
+
 <style>
     div { width: 100%; height: 100%; }
 </style>
